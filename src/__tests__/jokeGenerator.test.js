d9461563b23e4e7d0f8c0ae137acd8f996647a91
djstephenson7 <dan_0502@hotmail.co.uk>
2018-12-04 13:02:08 +0000

    Passed first test

--- src/__tests__/jokeGenerator.test.js
+++ src/__tests__/jokeGenerator.test.js
@@ -1,5 +1,7 @@
 import { render } from "react-testing-library";
+import 'jest-dom/extend-expect'
 import React from 'react';
+import Joke from '../joke';
 
 test("Joke component receives props and then renders text", () => {
   const { getByTestId, getByText, container } = render(
--- src/joke.js
+++ src/joke.js
@@ -1,5 +1,8 @@
-export default ({ text } =>
-<div data-testid="joke-text">
+import React from 'react';
+
+export default ({ text }) => (
+  <div data-testid="joke-text">
   {text}
+
 </div>
 );
