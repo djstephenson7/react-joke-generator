210abcd6316a5273a65adaab1cb64b404e6f161a
djstephenson7 <dan_0502@hotmail.co.uk>
2018-12-04 16:25:10 +0000

    Passed JokeGenerator test

--- src/__tests__/jokeGenerator.test.js
+++ src/__tests__/jokeGenerator.test.js
@@ -2,18 +2,19 @@ import { render } from "react-testing-library";
 import 'jest-dom/extend-expect'
 import React from 'react';
 import Joke from '../joke';
+import JokeGenerator from '../JokeGenerator'
 
 test("Joke component receives props and then renders text", () => {
   const { getByTestId, getByText, container } = render(
-    <Joke text="The funniest joke this year." />
+    <Joke text="Some joke." />
   );
 
   expect(getByTestId("joke-text")).toHaveTextContent(
-    "The funniest joke this year."
+    "Some joke."
   );
 });
 
 test("Joke generator fetches a random joke and renders it", () => {
   const { getByText } = render(<JokeGenerator />);
-  expect(getByText("You haven't loaded any joke yet!")).toBeInTheDOM();
+  expect(getByText("You haven't loaded any joke yet!")).toBeInTheDocument();
 })
---
+++ src/jokeGenerator.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class JokeGenerator extends React.Component {
+  render() {
+    return <div>
+    <h1>You haven't loaded any joke yet!</h1>
+    </div>
+  }
+}
