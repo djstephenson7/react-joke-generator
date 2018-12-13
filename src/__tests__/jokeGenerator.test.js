9e62fa5f0c2de7c6c6aa686857f3e7854559325d
djstephenson7 <dan_0502@hotmail.co.uk>
2018-12-13 19:10:38 +0000

    Used Axios for mocking

--- src/__tests__/jokeGenerator.test.js
+++ src/__tests__/jokeGenerator.test.js
@@ -1,8 +1,13 @@
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, wait } from "react-testing-library";
 import 'jest-dom/extend-expect'
+import * as axios from 'axios'
+import MockAxios from 'axios-mock-adapter'
 import React from 'react';
 import Joke from '../joke';
 import JokeGenerator from '../JokeGenerator'
+
+const mock = new MockAxios(axios, { delayResponse: Math.random() * 500 });
+afterAll(() => mock.restore());
 
 test("Joke component receives props and then renders text", () => {
   const { getByTestId, getByText, container } = render(
@@ -15,10 +20,19 @@ test("Joke component receives props and then renders text", () => {
 });
 
 test("Joke generator fetches a random joke and renders it", () => {
+  mock.onGet().replyOnce(200, {
+    value: {
+      joke: "Really funny joke!"
+    }
+  });
+
   const { getByText, queryByText } = render(<JokeGenerator />);
   expect(getByText("You haven't loaded any joke yet!")).toBeInTheDocument();
 
   fireEvent.click(getByText("Load a random joke."));
   expect(queryByText("You haven't loaded any joke yet!")).not.toBeInTheDocument();
   expect(queryByText("Loading...")).toBeInTheDocument();
+
+  await wait(() => expect(queryByText("Loading...")).not.toBeInTheDocument());
+  expect(queryByTestId("joke-text")).toBeInTheDocument();
 })
